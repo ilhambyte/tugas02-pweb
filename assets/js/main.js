b84ee1568ef684066a2f2a5591427b6e90795f78
ilhambyte <ilhamn14.a7x@gmail.com>
2022-10-10 09:17:44 +0700

    Ilham Nur Hermawan - 41215553

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,11 @@ menuTombol.addEventListener('click', function () {
     nav.classList.toggle('slide');
 });
 
+<<<<<<< HEAD
+// Tabel Klik
+=======
 
+>>>>>>> a070722ecbd25ea111987ee88750823e1a62b2b5
 $(document).ready(() => {
     $("td").click(function (event) {
         alert("Ada apa dengan " + $(this).html() + "?");
@@ -14,3 +18,24 @@ $(document).ready(() => {
         alert("Ada apa dengan " + $(this).html() + "?");
     });
 });
+
+// Anti COPAS, Inspect, Intip Source Code
+window.onload = function () {
+    document.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+    }, false);
+
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else if (window.event) {
+            window.event.cancelBubble = true;
+        }
+        e.preventDefault();
+        return false;
+    }
+};
+document.onkeydown = function (e) {
+    return false;
+}
+navigator.keyboard.lock();
